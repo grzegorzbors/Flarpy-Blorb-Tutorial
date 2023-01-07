cad7164f753258db1b57cca606027c8f0ced972f
grzegorzbors <gbors@o2.pl>
2023-01-07 18:15:23 +0100

    init code commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public GameObject bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && logic.birdIsAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+
+        CheckBirdOutOfBounds();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        Destroy(bird);
+    }
+
+    private void CheckBirdOutOfBounds()
+    {
+        if (bird.transform.position.y > 16 || bird.transform.position.y < -16)
+        {
+            logic.gameOver();
+            Destroy(bird);
+        }
+    }
+}
---
+++ Assets/LogicScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public bool birdIsAlive = true;
+
+    public AudioSource audioSource;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore += scoreToAdd;
+        scoreText.text = playerScore.ToString();
+        audioSource.Play();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        birdIsAlive = true;
+    }
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+        birdIsAlive = false;
+    }
+}
